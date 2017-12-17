3c7add8440fd1ea41521e4599c844f210d2d7f62
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2017-12-17 12:15:45 +0100

    format

--- src/ElfMagic.hs
+++ src/ElfMagic.hs
@@ -6,14 +6,12 @@ hasElfMagic :: L.ByteString -> Bool
 hasElfMagic cont = L.take 4 cont == elfMagic
                   where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
 
-isElfFile :: FilePath -> IO Bool
-{-
-the following function is equivalent to:
+{- the following function is equivalent to:
 ```
 L.readFile fname >>= return . hasElfMagic
 ```
 and is less verbose
-(suggested by haskell plugin)
--}
+(suggested by haskell plugin) -}
+isElfFile :: FilePath -> IO Bool
 isElfFile fname = hasElfMagic <$> L.readFile fname
 
