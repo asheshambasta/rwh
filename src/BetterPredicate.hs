5152204fbaa66d0f0f8c44063e285e3d26d2e6bc
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2017-12-19 09:54:00 +0100

    wip: file search

--- src/BetterPredicate.hs
+++ src/BetterPredicate.hs
@@ -3,20 +3,31 @@ module BetterPredicate where
 import RecursiveContents (recursiveContents)
 import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
-import System.Time (ClockTime(..))
+import Data.Time.Clock (UTCTime)
 import System.FilePath (takeExtension)
-import Control.Exception (bracket, handle)
+import Control.Exception (bracket, handle, SomeException)
 import System.IO(IOMode(..), hClose, hFileSize, openFile)
 
 type Predicate  = FilePath -- path to dir. entry
                 -> Permissions -- permissions
                 -> Maybe Integer -- file size, Nothing if dir
-                -> ClockTime -- last modified
+                -> UTCTime -- last modified
                 -> Bool
 
 getFileSize :: FilePath -> IO (Maybe Integer)
-getFileSize = undefined
+getFileSize path = handle ignoreErr $
+  bracket (openFile path ReadMode) hClose $ \h -> do
+    s <- hFileSize h
+    return (Just s)
+  where ignoreErr :: SomeException -> IO (Maybe Integer)
+        ignoreErr _ = return Nothing
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
-betterFind = undefined
+betterFind p path =
+  recursiveContents path >>= filterM check
+  where check name = do
+                perms <- getPermissions name
+                size <- getFileSize name
+                modified <- getModificationTime name
+                return (p name perms size modified)
 
