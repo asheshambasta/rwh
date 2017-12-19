bd72ecf49a5a46e72a9acca142cc3e49295664c1
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2017-12-19 21:00:59 +0100

    wip: predicates

--- src/BetterPredicate.hs
+++ src/BetterPredicate.hs
@@ -31,3 +31,57 @@ betterFind p path =
                 modified <- getModificationTime name
                 return (p name perms size modified)
 
+type InfoP a  =   FilePath
+              ->  Permissions
+              ->  Maybe Integer
+              ->  UTCTime
+              ->  a
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _  = size
+sizeP _ _ Nothing _      = -1
+
+pathP :: InfoP FilePath
+pathP p _ _ _  = p
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f a fp p s t = f fp p s t == a
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP abc infA b fp p i t = abc (infA fp p i t) b
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 abc ia ib fp p mi i = abc (ia fp p mi i) (ib fp p mi i) -- same as abc a b (which is  :: c)
+
+andP = liftP2 (&&)
+orP = liftP2 (||)
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath fa fp _ _ _ = fa fp
+
+myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP`
+          (sizeP `greaterP` 131072)
+
+(==?) :: (Eq a) => InfoP a -> a -> InfoP Bool
+(==?) = equalP
+
+(&&?) = andP
+
+(||?) = orP
+
+(>?) :: (Ord a) => InfoP a -> a -> InfoP Bool
+(>?) = greaterP
+
+(<?) :: (Ord a) => InfoP a -> a -> InfoP Bool
+(<?) = lesserP
+
+-- set precedence for comparators
+infix 4 ==?
+infix 3 &&?
+infix 4 >?
+infix 4 <?
+infix 2 ||?
