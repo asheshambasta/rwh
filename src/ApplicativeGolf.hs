769af156e1b1efd74e4bbf8fa806b73c8e03962d
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2018-01-02 11:38:00 +0100

    more sequence versions to exhibit conciseness vs readability

--- src/ApplicativeGolf.hs
+++ src/ApplicativeGolf.hs
@@ -3,3 +3,10 @@ module ApplicativeGolf where
 sequenceB_ :: (Applicative f) => [f a] -> f [a]
 sequenceB_ = foldr (\f1 -> (<*>) ((:) <$> f1)) (pure [])
 
+sequenceB2_ :: (Applicative f) => [f a] -> f [a]
+sequenceB2_ [] = pure []
+sequenceB2_ (f1:rest) = (:) <$> f1 <*> sequenceB2_ rest
+
+sequenceB3_ :: (Applicative f) => [f a] -> f [a]
+sequenceB3_ = foldr chain (pure [])
+    where chain f1 = (<*>) ((:) <$> f1)
