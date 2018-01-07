e8062e87666b2165dd33a0fe2ca2be9ea7186929
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2018-01-07 15:25:37 +0100

    just for trying out: monoid instance for Maybe a

--- src/MonoidGolf.hs
+++ src/MonoidGolf.hs
@@ -10,3 +10,16 @@ mult Unity (SomeVal y) = SomeVal y
 instance Monoid MValue where
   mempty = Unity
   mappend = mult
+
+
+lengthCompare :: String -> String -> Ordering
+lengthCompare s1 s2 = (l1 `compare` l2) `mappend` (s1 `compare` s2)
+  where l1 = length s1
+        l2 = length s2
+
+-- just an example of a Maybe Monoid
+{-instance Monoid a => Monoid (Maybe a) where
+  mempty = Nothing
+  Nothing `mappend` m = m
+  m `mappend` Nothing = m
+  (Just a1) `mappend` (Just a2) = Just (a1 `mappend` a2)-}
