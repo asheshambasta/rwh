dc6848a32ecf9bb55ed9b8e418226c15de14bbc0
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2018-01-11 21:27:22 +0100

    first steps with Parsec

---
+++ src/Csv1.hs
@@ -0,0 +1,32 @@
+module Csv1 where
+import Text.ParserCombinators.Parsec
+
+csvFile :: GenParser Char st [[String]]
+csvFile =
+  do result <- many line
+     eof
+     return result
+
+line :: GenParser Char st [String]
+line =
+  do result <- cells
+     eol
+     return result
+
+cells :: GenParser Char st [String]
+cells =
+  do first <- cellContent
+     next  <- remainingCells
+     return (first : next)
+
+remainingCells :: GenParser Char st [String]
+remainingCells = (char ',' >> cells) <|> return []
+
+cellContent :: GenParser Char st String
+cellContent = many (noneOf ",\n\r")
+
+eol :: GenParser Char st Char
+eol = oneOf "\n\r"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
