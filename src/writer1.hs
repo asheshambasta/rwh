2bbf47443ed30773839afe1a73b3a44a03e62edb
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2018-01-14 12:45:34 +0100

    simple GCD

--- src/writer1.hs
+++ src/writer1.hs
@@ -5,4 +5,14 @@ logNumber x =  writer (x, ["got number " ++ show x])
 multLog a b  = do
   la <- logNumber a
   lb <- logNumber b
+  tell [show a ++ " * " ++ show b]
   return (la * lb)
+
+
+gcd' a b
+    | b == 0 = do
+      tell ["finished with: " ++ show a]
+      return a
+    | otherwise = do
+      tell ["trying with " ++ show a ++ ", " ++ show b]
+      gcd' b (a `mod` b)
